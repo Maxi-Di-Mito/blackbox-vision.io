c4f84fabd8f5642e3f5bbb0ed705eedc78fdd9cc
jonatan <jonatansalas@live.com.ar>
2016-05-18 22:42:23 -0300

    Added nodemailer to package.json

--- src/public/dist/bundle.js
+++ src/public/dist/bundle.js
@@ -22612,8 +22612,6 @@
 	    value: true
 	});
 
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 	var _react = __webpack_require__(2);
 
 	var _react2 = _interopRequireDefault(_react);
@@ -22628,39 +22626,15 @@
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-	var Application = function (_Component) {
-	    _inherits(Application, _Component);
-
-	    function Application(props) {
-	        _classCallCheck(this, Application);
-
-	        return _possibleConstructorReturn(this, Object.getPrototypeOf(Application).call(this, props));
-	    }
-
-	    _createClass(Application, [{
-	        key: 'componentDidMount',
-	        value: function componentDidMount() {}
-	    }, {
-	        key: 'render',
-	        value: function render() {
-	            return _react2.default.createElement(
-	                'div',
-	                null,
-	                _react2.default.createElement(_LocaleSwitcher2.default, null),
-	                _react2.default.createElement(_Content2.default, { componentId: 'header-component', contentId: 'header1-text', contentType: 'text' }),
-	                _react2.default.createElement(_Content2.default, { componentId: 'header-component', contentId: 'header-image', contentType: 'image' })
-	            );
-	        }
-	    }]);
-
-	    return Application;
-	}(_react.Component);
+	var Application = function Application() {
+	    return _react2.default.createElement(
+	        'div',
+	        null,
+	        _react2.default.createElement('header', null),
+	        _react2.default.createElement('main', null),
+	        _react2.default.createElement('footer', null)
+	    );
+	};
 
 	exports.default = Application;
 
@@ -22804,7 +22778,7 @@
 	var mapDispatchToProps = function mapDispatchToProps(dispatch) {
 	    return {
 	        fetchContentData: function fetchContentData() {
-	            dispatch(fetchContent());
+	            //    dispatch(fetchContent());
 	        }
 	    };
 	};
--- src/shared/containers/Application.jsx
+++ src/shared/containers/Application.jsx
@@ -2,20 +2,12 @@ import React, { Component ,PropTypes } from 'react';
 import Content from '../components/Content.jsx';
 import LocaleSwitcher from '../components/LocaleSwitcher.jsx';
 
-class Application extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    componentDidMount() { }
-
-    render() {
-        return (
-            <div>
-                <LocaleSwitcher />
-            </div>
-        )
-    }
-}
+const Application = () => (
+    <div>
+        <header />
+        <main />
+        <footer />
+    </div>
+);
 
 export default Application;
