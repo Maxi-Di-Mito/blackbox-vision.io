2bd1c0cb789734ded013254c4949ed0ade7ce707
jonatan <jonatansalas@live.com.ar>
2016-05-22 12:49:42 -0300

    Updated section heights

--- src/shared/components/Body.jsx
+++ src/shared/components/Body.jsx
@@ -9,11 +9,11 @@ class Body extends Component {
         return (
         <div>
             <section id="#WhoWeAre" style={{ height: "600px"}}/>
-            <section id="#WhatWeDo" style={{ height: "600px", backgroundColor: "#e0e0e0"}}/>
-            <section id="#MeetTheTeam" style={{ height: "600px", backgroundColor: "#e0e0e0"}}/>
-            <section id="#Portfolio" style={{ height: "600px", backgroundColor: "#e0e0e0"}}/>
-            <section id="#Contact" style={{ height: "600px"}}/>
-            <section id="#Blog" style={{ height: "600px", backgroundColor: "#e0e0e0"}}/>
+            <section id="#WhatWeDo" style={{ height: "650px", backgroundColor: "#e0e0e0"}}/>
+            <section id="#MeetTheTeam" style={{ height: "650px", backgroundColor: "#e0e0e0"}}/>
+            <section id="#Portfolio" style={{ height: "650px", backgroundColor: "#e0e0e0"}}/>
+            <section id="#Contact" style={{ height: "650px"}}/>
+            <section id="#Blog" style={{ height: "650px", backgroundColor: "#e0e0e0"}}/>
         </div>
         )
     }
