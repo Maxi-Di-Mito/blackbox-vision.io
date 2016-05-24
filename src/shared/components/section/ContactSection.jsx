50a9a4f128b251e7575d9dee50c42c34830b4545
jonatan <jonatansalas@live.com.ar>
2016-05-23 23:34:09 -0300

    Added card for form components

--- src/shared/components/section/ContactSection.jsx
+++ src/shared/components/section/ContactSection.jsx
@@ -1,4 +1,5 @@
 import React, { Component ,PropTypes } from 'react';
+import { Card, CardTitle, CardText, CardActions, CardMenu, Button, IconButton, Grid, Cell } from 'react-mdl';
 
 class ContactSection extends Component {
     constructor(props) {
@@ -7,7 +8,23 @@ class ContactSection extends Component {
 
     render() {
         return (
-            <section id="Contact" style={{ height: "650px", backgroundColor: "#e0e0e0"}}/>
+            <section id="Contact" style={{ height: "650px", backgroundColor: "#ffa000"}}>
+                <Grid className="demo-grid-1">
+                    <Cell col={4}/>
+                    <Cell col={4}>
+                        <Card shadow={0} style={{ width: "100%", height: "100%" }}>
+                            <CardTitle>Contact</CardTitle>
+                            <CardText>
+                                sagittis pellentesque lacus eleifend lacinia...
+                            </CardText>
+                            <CardActions border>
+                                <Button colored ripple>Send</Button>
+                            </CardActions>
+                        </Card>
+                    </Cell>
+                    <Cell col={4}/>
+                </Grid>
+            </section>
         )
     }
 }
