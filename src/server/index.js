fefe893cdf2daed10f1f0dc1c1a1261f6fd14ff1
jonatan <jonatansalas@live.com.ar>
2016-05-28 18:26:25 -0300

    Updated server call Added react-router to the dependencies

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,10 @@
 import ServerConfig from './config/server.config';
-import server from './core/server';
 import mongoose from 'mongoose';
 import Winston from 'winston';
 
 mongoose.connect(ServerConfig.MONGO_URI).then(() => {
     Winston.log("info", "Connected at MongoDB -> " + ServerConfig.MONGO_URI);
-    server;
+    require('./core/server');
 }).catch((err) => {
     Winston.log("error", "An error happen when connecting to MongoDB -> " + err);
 });
