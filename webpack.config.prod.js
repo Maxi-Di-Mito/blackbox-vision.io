4318960f435ba1ab5d2f9dc18f1d0eef4323fad7
jonatan <jonatansalas@live.com.ar>
2016-06-08 09:24:31 -0300

    Deleted js for css delivery optimize Modified server to add plain script as string Modified package.json to add cross-env dependency Added webpack for production build Modified scripts in package.json for production build

---
+++ webpack.config.prod.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [path.join(__dirname, './src/client/index.jsx')],
+    output: {
+        path: path.join(__dirname, './src/public/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loaders: ['babel-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loader: 'file-loader?name=fonts/[name]/[name].[ext]',
+                exclude: /node_modules/
+            },
+            {
+                test   : /\.(jpg|png|gif)(\?[a-z0-9=&.]+)?$/,
+                loader: 'url-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: false,
+            mangle: false
+        })
+    ]
+};
