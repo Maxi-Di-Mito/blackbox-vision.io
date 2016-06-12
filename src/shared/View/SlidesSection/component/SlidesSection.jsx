ef66cfa8b1c158fca0316f8b72f9a8bb8b235de9
jonatan <jonatansalas@live.com.ar>
2016-06-11 23:49:28 -0300

    - Added message for SlidesSection - Added some css

--- src/shared/View/SlidesSection/component/SlidesSection.jsx
+++ src/shared/View/SlidesSection/component/SlidesSection.jsx
@@ -1,12 +1,16 @@
 import React, { Component ,PropTypes } from 'react';
 
-const SlidesSection = ({ imageUrl }) => (
-    <section id="Slides" style={{ height: "650px", background: 'url("assets/images/programming.png") center/cover' }}>
+const SlidesSection = (props) => (
+    <section id="Slides" className="slide-container">
+        <div className="slide-div">
+            <h1 className="mdl-color-text--white mdl-typography--text-uppercase mdl-typography--text-center mdl-typography--font-bold mdl-typography--display-3">
+                Let's Make your projects come true
+            </h1>
+            <h3 className="mdl-color-text--white mdl-typography--text-uppercase mdl-typography--text-center mdl-typography--font-medium mdl-typography--display-1-color-contrast">
+                Let us make the hardest part for you and get the product you were looking for
+            </h3>
+        </div>
     </section>
 );
-
-SlidesSection.propTypes = {
-    imageUrl: PropTypes.string.isRequired
-};
 
 export default SlidesSection;
