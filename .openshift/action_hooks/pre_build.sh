9af05c14c0e297843f8dc2ff84ee61c825dacc4c
jonatan <jonatansalas@live.com.ar>
2016-06-20 17:05:33 -0300

    Update build.sh

--- .openshift/action_hooks/pre_build.sh
+++ .openshift/action_hooks/pre_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 #NODE_VERSION_URL
 TARGET_NODE_VERSION=$(node -e "var p = require('$OPENSHIFT_REPO_DIR/package.json')||{engines:{}}; console.log(encodeURI(p.engines.node||''));")
 echo "https://semver.io/node/resolve/$TARGET_NODE_VERSION" > ${HOME}.env/user_vars/NODE_VERSION_URL
