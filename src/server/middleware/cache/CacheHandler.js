060054bfa4fb82ec7a1a0afcbcbb83ece96d0c65
jonatan <jonatansalas@live.com.ar>
2016-06-22 15:01:29 -0300

    Better way to handle caching

--- src/server/middleware/cache/CacheHandler.js
+++ src/server/middleware/cache/CacheHandler.js
@@ -1,7 +1,38 @@
-const CacheHandler = (request, response, next) => {
-    if (request.url.match(/^\/(css|img|js|font)\/.+/)) {
-        response.header('Cache-Control', 'public, max-age=3600');
+const cacheMap = [
+    {
+        type: "image",
+        pattern: /\.(jpg|jpeg|png)$/,
+        cacheTime: "36288000"
+    },
+    {
+        type: "video",
+        pattern: /\.(mp4)$/,
+        cacheTime: "36288000"
+    },
+    {
+        type: "style",
+        pattern: /\.(css)$/,
+        cacheTime: "36288000"
+    },
+    {
+        type: "font",
+        pattern: /\.(woff|woff2|ttf|eot|svg)$/,
+        cacheTime: "36288000"
+    },
+    {
+        type: "script",
+        pattern: /\.(js)$/,
+        cacheTime: "36288000"
     }
+];
+
+const CacheHandler = (request, response, next) => {
+    cacheMap.forEach((cacheData) => {
+        if(request.url.match(cacheData.pattern)) {
+            //console.log("Caching " + cacheData.type);
+            response.header('Cache-Control', `public, max-age=${cacheData.cacheTime}`);
+        }
+    });
 
     return next();
 };
