0752c2289132f785838555d1c17462fc806a7307
jonatansalas <jonatansalas@live.com.ar>
2016-11-02 20:02:54 -0300

    Added Html component, and removed RenderHelper. Added StyleSheet to do styled-components server side render.

--- src/server/middleware/Middlewares.js
+++ src/server/middleware/Middlewares.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import styleSheet from 'styled-components/lib/models/StyleSheet'
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import { match } from 'react-router';
-import RenderHelper from '../helper/RenderHelper';
 import configureStore from '../../shared/redux/store/configureStore';
 import routes from '../../shared/routes.js';
 import App from '../../shared/App';
+import Html from '../../shared/components/enviroments/Html';
 
 class Middlewares {
     static handleRender(request, response, next) {
         let store = configureStore();
 
         match({ routes, location: request.url }, (error, redirectLocation, renderProps) => {
-            if (error) return next(error);
-            if (!renderProps) return next();
-            if (redirectLocation) return response.redirect(302, redirectLocation.pathname + redirectLocation.search);
+            if (error)
+                return next(error);
 
-            let initialHtml = ReactDOMServer.renderToString(
+            if (!renderProps)
+                return next();
+
+            if (redirectLocation)
+                return response.redirect(302, redirectLocation.pathname + redirectLocation.search);
+
+            const content = ReactDOMServer.renderToString(
                 <App
                     store={store}
                     renderProps={renderProps}
@@ -30,10 +36,19 @@ class Middlewares {
                 />
             );
 
-            return response
-                .status(200)
-                .header("Content-Type", "text/html; charset=utf-8")
-                .end(RenderHelper.toDefaultHtml(initialHtml, store.getState()));
+            const styles = styleSheet.rules().map(rule => rule.cssText).join('\n');
+            const html = (
+                <Html
+                    content={content}
+                    state={store.getState()}
+                    styles={styles}
+                />
+            );
+
+            response.status(200);
+            response.header('Content-Type", "text/html; charset=utf-8');
+            response.send(`<!doctype html>\n${ReactDOMServer.renderToStaticMarkup(html)}`);
+            response.end();
         })
     }
 
--- src/shared/routes.js
+++ src/shared/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Route from 'react-router/lib/Route';
 import IndexRoute from 'react-router/lib/IndexRoute';
-import Main from './components/enviroments/Main.js';
+import Main from './components/ecosystems/Main.js';
 import Website from './components/pages/Website.js';
 
 const routes = (
