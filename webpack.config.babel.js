a29ea25e9a7c6509c568d366e1798ef6658561c4
jonatansalas <jonatansalas@live.com.ar>
2016-11-01 23:56:44 -0300

    Minor changes to webpack files

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,3 @@
-var webpack = require('webpack');
-
 module.exports = {
     output: {
         publicPath: '/',
@@ -19,13 +17,5 @@ module.exports = {
               loader: 'url-loader?limit=10000',
            },
         ],
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('development'),
-                BABEL_DISABLE_CACHE: JSON.stringify(1)
-            }
-        })
-    ]
+    }
 };
